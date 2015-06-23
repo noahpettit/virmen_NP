d769a04b28425f2c1aa1bc8d5d637bc79bac9868
Selmaan <chettih@fas.harvard.edu>
2015-06-23 19:19:59 -0400

    added incorrect-penalty maze

--- generalFunctions/chooseNextWorld.m
+++ generalFunctions/chooseNextWorld.m
@@ -1,8 +1,14 @@
 function vr = chooseNextWorld(vr,worlds)
-
+    
 if isempty(worlds)
     vr.currentWorld = randi(vr.nWorlds);
 else
+    if isfield(vr,'penaltyProb')
+       if ~ismember(vr.numTrials, vr.sessionSwitchpoints) & ~vr.isReward
+            worlds(end+1:end+vr.penaltyProb) = vr.currentWorld,
+       end  
+    end
+    
     worldChoice = randi(length(worlds));
     vr.currentWorld = worlds(worldChoice);
 end
