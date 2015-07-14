242eb496b23e039c1334e1f869e892ad6c497328
Selmaan <chettih@fas.harvard.edu>
2015-07-14 11:27:45 -0400

    waitForNextTrial now works with debug mode

--- generalFunctions/waitForNextTrial.m
+++ generalFunctions/waitForNextTrial.m
@@ -4,17 +4,19 @@ if vr.inITI == 1
     vr.itiTime = toc(vr.itiStartTime);
     if vr.itiTime > vr.itiDur
         if ~vr.debugMode
-            global mvData
-            if norm(mvData - [1.65 1.65]) < vr.mvThresh
-                vr.inITI = 0;
-                vr = chooseNextWorld(vr);
-                vr.position = vr.worlds{vr.currentWorld}.startLocation;
-                vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
-                vr.dp = 0;
-                vr.firstTurn = 1;
-                vr.trialTimer = tic;
-                vr.trialStartTime = rem(now,1);
-            end
+            isMouseStill = norm(vr.velocity) < vr.mvThresh;
+        else
+            isMouseStill = 1;
+        end
+        if isMouseStill
+            vr.inITI = 0;
+            vr = chooseNextWorld(vr);
+            vr.position = vr.worlds{vr.currentWorld}.startLocation;
+            vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
+            vr.dp = 0;
+            vr.firstTurn = 1;
+            vr.trialTimer = tic;
+            vr.trialStartTime = rem(now,1);
         end
     end
 end
