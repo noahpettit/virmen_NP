a202f9adeb6938e2e52352ed7f0deffcdd95b75e
mjlm <m.minderer@gmail.com>
2015-07-13 21:36:41 -0400

    debug and calibrate new sensor

--- generalFunctions/waitForNextTrial.m
+++ generalFunctions/waitForNextTrial.m
@@ -3,18 +3,15 @@ function vr = waitForNextTrial(vr)
 if vr.inITI == 1
     vr.itiTime = toc(vr.itiStartTime);
     if vr.itiTime > vr.itiDur
-        if ~vr.debugMode
-            global mvData
-            if norm(mvData - [1.65 1.65]) < vr.mvThresh
-                vr.inITI = 0;
-                vr = chooseNextWorld(vr);
-                vr.position = vr.worlds{vr.currentWorld}.startLocation;
-                vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
-                vr.dp = 0;
-                vr.firstTurn = 1;
-                vr.trialTimer = tic;
-                vr.trialStartTime = rem(now,1);
-            end
+        if norm(vr.velocity) < vr.mvThresh
+            vr.inITI = 0;
+            vr = chooseNextWorld(vr);
+            vr.position = vr.worlds{vr.currentWorld}.startLocation;
+            vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
+            vr.dp = 0;
+            vr.firstTurn = 1;
+            vr.trialTimer = tic;
+            vr.trialStartTime = rem(now,1);
         end
     end
 end
