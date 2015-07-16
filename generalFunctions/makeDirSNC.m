776df38e114ffa76da7682dc3cf22258790966e3
Selmaan <chettih@fas.harvard.edu>
2015-07-16 18:44:39 -0400

    Changed save directory from Mary to Annie

--- generalFunctions/makeDirSNC.m
+++ generalFunctions/makeDirSNC.m
@@ -6,7 +6,7 @@ if vr.debugMode
 else    
     mouseInfo = inputdlg('Input the Mouse Number: ');
     vr.mouseNum = str2double(mouseInfo{1});
-    vr.basePath = 'C:\DATA\Mary';
+    vr.basePath = 'C:\DATA\Annie';
 end
 vr.date = date;
 vr.fullPath = [vr.basePath filesep num2str(vr.mouseNum) filesep vr.date];
