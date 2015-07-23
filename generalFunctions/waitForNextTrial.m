f7f45411b66a7c48834ebe5002dda0bf9539c8f9
mjlm <m.minderer@gmail.com>
2015-07-23 10:48:32 -0400

    Revert "toRevert"
    This reverts commit c8e99850e1cfa3fa90d9fb2d704e2cc349e6f508.

--- generalFunctions/waitForNextTrial.m
+++ generalFunctions/waitForNextTrial.m
@@ -1,19 +1,22 @@
-function vr = waitForNextTrial(vr,worlds)
-
-if ~exist('worlds','var')
-    worlds = [];
-end
+function vr = waitForNextTrial(vr)
 
 if vr.inITI == 1
     vr.itiTime = toc(vr.itiStartTime);
     if vr.itiTime > vr.itiDur
-        vr.inITI = 0;
-        vr = chooseNextWorld(vr,worlds);
-        vr.position = vr.worlds{vr.currentWorld}.startLocation;
-        vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
-        vr.dp = 0;
-        vr.firstTurn = 1;
-        vr.trialTimer = tic;
-        vr.trialStartTime = rem(now,1);
+        if ~vr.debugMode
+            isMouseStill = norm(vr.velocity) < vr.mvThresh;
+        else
+            isMouseStill = 1;
+        end
+        if isMouseStill
+            vr.inITI = 0;
+            vr = chooseNextWorld(vr);
+            vr.position = vr.worlds{vr.currentWorld}.startLocation;
+            vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
+            vr.dp = 0;
+            vr.firstTurn = 1;
+            vr.trialTimer = tic;
+            vr.trialStartTime = rem(now,1);
+        end
     end
 end
