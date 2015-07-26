a37480c1b1a2771f28731c3117283841ded7a476
mjlm <m.minderer@gmail.com>
2015-07-26 19:01:27 -0400

    to revert

--- generalFunctions/endVRTrial.m
+++ generalFunctions/endVRTrial.m
@@ -1,4 +1,11 @@
-function vr = endVRTrial(vr)
+function vr = endVRTrial(vr,rewarded)
+
+if rewarded
+    vr = giveReward(vr,1);
+    vr.itiDur = vr.itiCorrect;
+else
+    vr.itiDur = vr.itiMiss;
+end
 
 vr.worlds{vr.currentWorld}.surface.visible(:) = 0;
 vr.itiStartTime = tic;
--- generalFunctions/makeDirSNC.m
+++ generalFunctions/makeDirSNC.m
@@ -1,13 +1,8 @@
 function vr = makeDirSNC(vr)
 
-if vr.debugMode
-    vr.mouseNum = randi(1e6,1)+1e3;
-    vr.basePath = 'C:\DATA\Debug';
-else    
-    mouseInfo = inputdlg('Input the Mouse Number: ');
-    vr.mouseNum = str2double(mouseInfo{1});
-    vr.basePath = 'C:\DATA\Annie';
-end
+mouseInfo = inputdlg('Input the Mouse Number: ');
+vr.mouseNum = str2double(mouseInfo{1});
+vr.basePath = 'C:\DATA\Mary';
 vr.date = date;
 vr.fullPath = [vr.basePath filesep num2str(vr.mouseNum) filesep vr.date];
 
