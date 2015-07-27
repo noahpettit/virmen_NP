8e5588809881f60fc903da6110437a3210e43fc0
Selmaan <chettih@fas.harvard.edu>
2015-07-27 17:14:14 -0400

    toRevert

--- generalFunctions/initCounters.m
+++ generalFunctions/initCounters.m
@@ -6,7 +6,5 @@ vr.numRewards = 0;
 vr.dp = 0;
 vr.isReward = 0;
 vr.trialIterations = 0;
-vr.wrongStreak = 0;
-vr.inRewardZone = 0;
 vr.sessionStartTime = tic;
 vr.behaviorData = nan(9,1e4);
--- generalFunctions/waitForNextTrial.m
+++ generalFunctions/waitForNextTrial.m
@@ -1,22 +1,19 @@
-function vr = waitForNextTrial(vr)
+function vr = waitForNextTrial(vr,worlds)
+
+if ~exist('worlds','var')
+    worlds = [];
+end
 
 if vr.inITI == 1
     vr.itiTime = toc(vr.itiStartTime);
     if vr.itiTime > vr.itiDur
-        if ~vr.debugMode
-            isMouseStill = norm(vr.velocity) < vr.mvThresh;
-        else
-            isMouseStill = 1;
-        end
-        if isMouseStill
-            vr.inITI = 0;
-            vr = chooseNextWorld(vr);
-            vr.position = vr.worlds{vr.currentWorld}.startLocation;
-            vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
-            vr.dp = 0;
-            vr.inRewardZone = 0;
-            vr.trialTimer = tic;
-            vr.trialStartTime = rem(now,1);
-        end
+        vr.inITI = 0;
+        vr = chooseNextWorld(vr,worlds);
+        vr.position = vr.worlds{vr.currentWorld}.startLocation;
+        vr.worlds{vr.currentWorld}.surface.visible(:) = 1;
+        vr.dp = 0;
+        vr.firstTurn = 1;
+        vr.trialTimer = tic;
+        vr.trialStartTime = rem(now,1);
     end
 end
