484eca9f3b81420a83495b13707ea6dd11b39182
Selmaan <smaaan@gmail.com>
2015-08-08 15:40:01 -0400

    Cleaned up hiding-target code

--- software/ViRMEn 2015-03-19/experiments/SC_switchingChambers_hideTarget.m
+++ software/ViRMEn 2015-03-19/experiments/SC_switchingChambers_hideTarget.m
@@ -42,24 +42,7 @@ vr = initCounters(vr);
 vr = createBlockStructure(vr,initBlock);
 
 % Identify indices for cue target walls (for hiding)
-for worldNum = 5:8
-    objID = vr.worlds{worldNum}.objects.indices.targetSideWalls_hide;
-    objVert = vr.worlds{worldNum}.objects.vertices(objID,:);
-    cueVert = objVert(1):objVert(2);
-    objID = vr.worlds{worldNum}.objects.indices.targetRearWall_hide;
-    objVert = vr.worlds{worldNum}.objects.vertices(objID,:);
-    cueVert = [cueVert, objVert(1):objVert(2)];
-    vr.cueToHide{worldNum} = cueVert;
-    objID = vr.worlds{worldNum}.objects.indices.targetSideWalls;
-    objVert = vr.worlds{worldNum}.objects.vertices(objID,:);
-    blankVert = objVert(1):objVert(2);
-    objID = vr.worlds{worldNum}.objects.indices.targetRearWall;
-    objVert = vr.worlds{worldNum}.objects.vertices(objID,:);
-    blankVert = [blankVert, objVert(1):objVert(2)];
-    vr.blankToHide{worldNum} = blankVert;
-    vr.worlds{worldNum}.surface.vertices(2,vr.blankToHide{worldNum}) =...
-        100 + vr.worlds{worldNum}.surface.vertices(2,vr.blankToHide{worldNum});
-end
+vr = getHidingTargetVertices(vr);
 
 % Choose the first world
 worldChoice = randi(size(vr.contingentBlocks,2));
