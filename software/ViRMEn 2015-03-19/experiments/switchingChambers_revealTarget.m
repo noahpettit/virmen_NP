25871ce7858c24e1e193c926316a0659d16c6515
selmaan_AnnieComp <smaaan@gmail.com>
2015-08-19 17:29:44 -0400

    added reveal cue fraction mazes (dots and gratings)
    miss ITI = 2
    stem length = 300
    fracNoCheck = 1
    revealCue = 50

--- software/ViRMEn 2015-03-19/experiments/switchingChambers_revealTarget.m
+++ software/ViRMEn 2015-03-19/experiments/switchingChambers_revealTarget.m
@@ -1,4 +1,4 @@
-function code = SC_switchingChambers_penalty
+function code = SC_switchingChambers_hideTarget
 % Linear Track   Code for the ViRMEn experiment Linear Track.
 %   code = Linear Track   Returns handles to the functions that ViRMEn
 %   executes during engine initialization, runtime and termination.
@@ -22,9 +22,9 @@ vr = makeDirSNC(vr);
 vr.rewardDelay = 1;
 vr.mvThresh = 10;
 vr.friction = 0.25;
-vr.itiCorrect = 0; % decreased when length of movement threshold increased 7/31
-vr.itiMissBase = 2; % added 8/14 (after training)AH
-vr.penaltyITI = 0; % decreased when length of movement threshold increased 7/31
+vr.itiCorrect = 0; 
+vr.itiMissBase = 2; 
+vr.penaltyITI = 0; 
 vr.penaltyProb = 0;
 floorLength = eval(vr.exper.variables.floorLength);
 funnelLength = eval(vr.exper.variables.funnelLength);
@@ -32,21 +32,24 @@ vr.rewardLength = 5 + floorLength + funnelLength;
 vr.nWorlds = length(vr.worlds);
 vr.sessionSwitchpoints = eval(vr.exper.variables.switches);
 vr.fractionNoChecker = eval(vr.exper.variables.fractionNoChecker);
+vr.hideCuePast = eval(vr.exper.variables.hideCuePast);
+initBlock = 2 - mod(vr.mouseNum,2);
 
+% General setup functions
 vr = initTextboxes(vr);
 vr = initDAQ(vr);
 vr = initCounters(vr);
+vr = createBlockStructure(vr,initBlock);
 
-nBlocks = length(vr.sessionSwitchpoints)+1;
-initBlock = 2 - mod(vr.mouseNum,2);
-blockIDs = mod(initBlock:initBlock+nBlocks-1,2) + 1;
-blockMazes = [1 2;3 4];
-for nBlock = 1:nBlocks
-    vr.contingentBlocks(nBlock,:) = blockMazes(blockIDs(nBlock),:);
-end
+% Identify indices for cue target walls (for hiding)
+vr = getHidingTargetVertices(vr);
 
+% Choose the first world
 worldChoice = randi(size(vr.contingentBlocks,2));
 vr.currentWorld = vr.contingentBlocks(1,worldChoice);
+if rand < vr.fractionNoChecker
+    vr.currentWorld = vr.currentWorld + 4;
+end
 
 % --- RUNTIME code: executes on every iteration of the ViRMEn engine.
 function vr = runtimeCodeFun(vr)
@@ -58,7 +61,7 @@ vr = collectBehaviorIter(vr);
 vr = adjustFriction(vr);
 
 % check for trial-terminating position and deliver reward
-if vr.inITI == 0 && (vr.position(2) > vr.rewardLength);
+if vr.inITI == 0 && (vr.position(2) > vr.rewardLength)
     % Disable movement
     vr.dp = 0*vr.dp;
     % Enforce Reward Delay
@@ -102,7 +105,24 @@ if vr.inITI == 0 && (vr.position(2) > vr.rewardLength);
     end
 else
     vr.behaviorData(9,vr.trialIterations) = 0;
-end    
+end
+
+% Check is position is past hide-point
+if (vr.currentWorld > 4) && ~vr.targetHidden && (vr.position(2) > vr.hideCuePast)
+    vr.targetHidden = 1;
+    % Hide cue walls in target zone
+    vr.worlds{vr.currentWorld}.surface.vertices(2,vr.cueToHide{vr.currentWorld}) = ...
+        100 + vr.worlds{vr.currentWorld}.surface.vertices(2,vr.cueToHide{vr.currentWorld});
+    vr.worlds{vr.currentWorld}.surface.vertices(2,vr.blankToHide{vr.currentWorld}) = ...
+        -100 + vr.worlds{vr.currentWorld}.surface.vertices(2,vr.blankToHide{vr.currentWorld});
+elseif vr.targetHidden && vr.inITI
+    vr.targetHidden = 0;
+    % Reset / reveal cue ID once in reward zone
+    vr.worlds{vr.currentWorld}.surface.vertices(2,vr.cueToHide{vr.currentWorld}) = ...
+        -100 + vr.worlds{vr.currentWorld}.surface.vertices(2,vr.cueToHide{vr.currentWorld});
+    vr.worlds{vr.currentWorld}.surface.vertices(2,vr.blankToHide{vr.currentWorld}) = ...
+        100 + vr.worlds{vr.currentWorld}.surface.vertices(2,vr.blankToHide{vr.currentWorld});
+end
 
 % Check to see if ITI has elapsed, and restart trial if it has
 vr = waitForNextTrial(vr);
@@ -116,6 +136,6 @@ if ~vr.debugMode
     stop(vr.ai),
     delete(vr.ai),
     delete(vr.ao),
+    [vr,sessionData] = collectTrialData(vr);
+    vr = makeSwitchingSessionFigs(vr,sessionData, vr.sessionSwitchpoints);
 end
-[vr,sessionData] = collectTrialData(vr);
-vr = makeSwitchingSessionFigs(vr,sessionData, vr.sessionSwitchpoints);
